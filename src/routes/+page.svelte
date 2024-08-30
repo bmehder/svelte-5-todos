b60745fea7bc49f5ee9d4cd397907a602c56b6dd
bmehder <bmehder@gmail.com>
2024-08-30 12:48:39 +0100

    shape outside

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,16 @@
 
 	const FILTERS = ['All', 'Completed', 'Remaining']
 
-	const filterTodos = x =>
+	// State Mutations
+	const filterTodos = option =>
 		new Map([
 			['All', todos],
-			['Completed', todos.filter(({ done }) => done)],
-			['Remaining', todos.filter(({ done }) => !done)],
-		]).get(x)
+			['Completed', todos.filter(({ isDone }) => !!isDone)],
+			['Remaining', todos.filter(({ isDone }) => !isDone)],
+		]).get(option)
 
-	const setFilter = x => {
-		filter = x
+	const setFilter = option => {
+		filter = option
 	}
 
 	const addTodo = evt => {
@@ -20,7 +21,7 @@
 		const todoEl = evt.target
 		const text = todoEl.value
 
-		todos.push({ text, done: false })
+		todos.push({ text, isDone: false })
 		todoEl.value = ''
 	}
 
@@ -32,7 +33,7 @@
 	}
 
 	const deleteTodo = evt => {
-		if (confirm('You sure?')) {
+		if (confirm('Are you sure you want to delete this todo?')) {
 			const inputEl = evt.target
 			const index = +inputEl.dataset.index
 
@@ -40,11 +41,11 @@
 		}
 	}
 
-	const toggleDone = evt => {
+	const toggleIsDone = evt => {
 		const inputEl = evt.target
 		const index = +inputEl.dataset.index
 
-		todos[index].done = !todos[index].done
+		todos[index].isDone = !todos[index].isDone
 	}
 
 	// State
@@ -52,7 +53,7 @@
 
 	let filter = $state('All')
 
-	const remaining = $derived(todos.filter(({ done }) => !done).length)
+	const remaining = $derived(todos.filter(({ isDone }) => !isDone).length)
 
 	const filteredTodos = $derived(filterTodos(filter))
 
@@ -80,11 +81,11 @@
 	<input class="p-0-5" onkeydown={addTodo} placeholder="Add new todo..." />
 
 	<div class="flex gap-1">
-		{#each FILTERS as _filter}
+		{#each FILTERS as option}
 			<button
 				class="bg-lime-4 gray-12 radius-1"
-				class:bg-lime-6={filter === _filter}
-				onclick={() => setFilter(_filter)}>{_filter}</button
+				class:bg-lime-6={filter === option}
+				onclick={() => setFilter(option)}>{option}</button
 			>
 		{/each}
 	</div>
@@ -93,15 +94,15 @@
 		{#each filteredTodos as todo, i (todo)}
 			<li
 				class="flex space-between gap-1 p-1 overlay-6 border-bottom-2"
-				class:gray-6={todo.done}
+				class:gray-6={todo.isDone}
 				transition:slide
 			>
 				<input
 					style="width: var(--size-1-5); accent-color: var(--lime-4)"
 					type="checkbox"
-					onchange={toggleDone}
+					onchange={toggleIsDone}
 					data-index={i}
-					checked={todo.done}
+					checked={todo.isDone}
 				/>
 				<input
 					class="full-width p-0-5 unset"
@@ -121,3 +122,38 @@
 
 	<div class="text-center h4">{remaining} remaining</div>
 </div>
+
+<section class="boxed py-2">
+	<img src="/woman.png" alt="" />
+	<p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia qui fuga sint
+		rerum iure molestiae cumque perferendis, quam dolor omnis ab! Commodi, et, quas
+		nisi consequuntur aperiam ex aut hic odit obcaecati delectus quo voluptatum, amet
+		vero vitae nobis minus in eveniet ipsum ratione rem pariatur soluta! Maiores
+		nesciunt magni ullam assumenda ipsa, quas earum? Illo exercitationem distinctio
+		cumque commodi nemo in quia omnis consequatur sed aliquam dolor repellendus
+		sapiente culpa hic ut quae doloribus perspiciatis quaerat temporibus, numquam
+		excepturi. Nam voluptate earum soluta sed itaque veniam provident illo
+		repellendus inventore dicta esse asperiores delectus quisquam fugiat ipsam
+		incidunt accusamus eveniet quasi, quas quis quod aspernatur ipsa, tempore fuga.
+		Sequi, magnam, hic animi dolore eligendi natus quas cupiditate nam quasi dolor
+		vero minus minima ratione. Repellendus modi pariatur, dignissimos, harum iure
+		totam labore, neque libero error ut illum a. Delectus sed ducimus, facere
+		asperiores fuga modi nulla velit distinctio repellat possimus error saepe
+		repellendus aut odio veniam quibusdam nihil quo quasi aliquid consequuntur.
+		Minima assumenda quam, sunt commodi quae, ea voluptas minus aperiam id delectus
+		blanditiis repellat a dolor doloremque dicta consectetur explicabo unde mollitia
+		facilis! Iusto voluptatem adipisci mollitia blanditiis similique distinctio.
+		Tempore maiores neque, accusantium molestias enim reprehenderit.
+	</p>
+</section>
+
+<style>
+	img {
+		float: right;
+		width: 50vw;
+		max-width: 400px;
+		shape-outside: url(/woman.png);
+		shape-margin: 2rem;
+	}
+</style>
